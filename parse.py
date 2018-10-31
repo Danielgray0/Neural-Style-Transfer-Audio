207b411c2332f2e8ff3408fddb7142927e50ec9d
Daniel Gray <daniel.gray@pop.belmont.edu>
2018-10-31 11:24:30 -0500

    added abstract and code for Hooper to review

--- parse.py
+++ parse.py
@@ -3,13 +3,13 @@
     Execution: python parse.py [file to print to] """
 
 from music21 import *
-example = converter.parse('chp_op18.mid')
+example = converter.parse('Midi_Data unused/chp_op18_new.mid')
 data = example.parts[0]
-# data.show()
-
+#data.show()
+#print(data.show())
 print ("%s,%s,%s,%s, %s" % ("Note/Rest", "Octave", "Len","Offset", "Velocity"))
 notes = [i for i in data.notesAndRests]
-Song=[]
+Song =[]
 for n in notes:
     if isinstance(n, note.Note):
         # alternative n.pitch.nameWithOctave
